7ab1fbf8827835576fbffa390accece9b3b39101
favorable-device-5158 <blee2125@gmail.com>
2019-08-15 16:18:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each {|person| badges<<"Hello, my name is #{person}."}
+  badges
+end
+
+def assign_rooms(attendees)
+  rooms=[]
+  attendees.each_with_index {|person, index| rooms<<"Hello, #{person}! You'll be assigned to room #{index+1}!"}
+  rooms
+end
+
+def printer(attendees)
+  attendees.each do
+
+  end
+end
