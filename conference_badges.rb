56e7548a070ddc62e428ca1e68dfca9d5a4bc489
favorable-device-5158 <blee2125@gmail.com>
2019-08-15 16:25:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  attendees.each do
-
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |rooms|
+    puts rooms
   end
 end
